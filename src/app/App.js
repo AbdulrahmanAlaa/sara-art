637cb9cfe2e60a2b34e47b06d8df05dc5277724b
Abdulrahman Alaa Eldin <abdulrahmanalaa@hotmail.com>
2025-04-11 21:42:38 +0400

    Update base URL handling in index.html and App.js for improved deployment flexibility

--- src/app/App.js
+++ src/app/App.js
@@ -21,7 +21,7 @@ const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename={'/sara-art'}>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="cursor__dot">
         <AnimatedCursor
           innerSize={15}
