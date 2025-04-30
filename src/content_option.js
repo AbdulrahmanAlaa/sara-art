8f9e1f005ff734a4297f2a48bce917ba03f701e6
Abdulrahman Alaa Eldin <abdulrahmanalaa@hotmail.com>
2025-05-01 03:58:10 +0400

    adding images

--- src/content_option.js
+++ src/content_option.js
@@ -72,28 +72,535 @@ const services = [{
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
 },
 ];
-import proj1_1 from "./assets/images/project1/1.png";
-import proj1_2 from "./assets/images/project1/2.png";
-import proj1_3 from "./assets/images/project1/3.png";
-const dataportfolio = [{
-    img: proj1_1,
-    id: '1',
-    description: "Character design",
-    link: "#",
-}
+
+// balerina
+import balerina from "./assets/images/projects/balerina/1.png";
+import balerina2 from "./assets/images/projects/balerina/2.png";
+import balerina3 from "./assets/images/projects/balerina/3.png";
+
+//bedro
+import bedr from "./assets/images/projects/bedro/1.PNG";
+import bedr2 from "./assets/images/projects/bedro/2.PNG";
+import bedr3 from "./assets/images/projects/bedro/3.PNG";
+import bedr4 from "./assets/images/projects/bedro/4.PNG";
+import bedr5 from "./assets/images/projects/bedro/5.PNG";
+
+// china 
+import china1 from "./assets/images/projects/china/1.JPG";
+import china2 from "./assets/images/projects/china/2.JPG";
+import china3 from "./assets/images/projects/china/3.JPG";
+import china4 from "./assets/images/projects/china/4.JPG";
+import china5 from "./assets/images/projects/china/5.JPG";
+
+// diamond box
+import diamondbox1 from "./assets/images/projects/diamond_box/1.png";
+import diamondbox2 from "./assets/images/projects/diamond_box/2.jpg";
+import diamondbox3 from "./assets/images/projects/diamond_box/3.png";
+import diamondbox4 from "./assets/images/projects/diamond_box/4.png";
+import diamondbox5 from "./assets/images/projects/diamond_box/5.png";
+import diamondbox6 from "./assets/images/projects/diamond_box/6.png";
+
+// emily
+import emily1 from "./assets/images/projects/emily/1.png";
+import emily2 from "./assets/images/projects/emily/2.png";
+import emily3 from "./assets/images/projects/emily/3.png";
+import emily4 from "./assets/images/projects/emily/4.gif";
+
+// gang 
+import gang1 from "./assets/images/projects/gang/All 2.png";
+import gang2 from "./assets/images/projects/gang/Amgad.png";
+import gang3 from "./assets/images/projects/gang/Amola.png";
+import gang4 from "./assets/images/projects/gang/Ash.png";
+import gang5 from "./assets/images/projects/gang/Eman.png";
+import gang6 from "./assets/images/projects/gang/Lofi.png";
+import gang7 from "./assets/images/projects/gang/Mahmoud.png";
+import gang8 from "./assets/images/projects/gang/Marium.png";
+import gang9 from "./assets/images/projects/gang/Nahla.png";
+import gang10 from "./assets/images/projects/gang/Namola.png";
+import gang11 from "./assets/images/projects/gang/Nemo.png";
+import gang12 from "./assets/images/projects/gang/Omnia.png";
+import gang13 from "./assets/images/projects/gang/Rony.png";
+import gang14 from "./assets/images/projects/gang/Shimo.png";
+import gang15 from "./assets/images/projects/gang/Salma.png";
+import gang16 from "./assets/images/projects/gang/Sara.png";
+import gang17 from "./assets/images/projects/gang/Tamtam.png";
+import gang18 from "./assets/images/projects/gang/Tete.png";
+
+// gentle_meao 
+import gentle_meao1 from "./assets/images/projects/gentle_meao/1.jpg";
+import gentle_meao2 from "./assets/images/projects/gentle_meao/2.jpg";
+import gentle_meao3 from "./assets/images/projects/gentle_meao/3.jpg";
+import gentle_meao4 from "./assets/images/projects/gentle_meao/4.jpg";
+import gentle_meao5 from "./assets/images/projects/gentle_meao/5.jpg";
+import gentle_meao6 from "./assets/images/projects/gentle_meao/6.jpg";
+import gentle_meao7 from "./assets/images/projects/gentle_meao/7.jpg";
+
+// ghet al regal
+import ghet_al_regal1 from "./assets/images/projects/ghet al regal/1.png";
+import ghet_al_regal2 from "./assets/images/projects/ghet al regal/2.png";
+import ghet_al_regal3 from "./assets/images/projects/ghet al regal/3.png";
+import ghet_al_regal4 from "./assets/images/projects/ghet al regal/4.png";
+import ghet_al_regal5 from "./assets/images/projects/ghet al regal/5.png";
+
+// jawab
+import jawab1 from "./assets/images/projects/jawab/1.png";
+import jawab2 from "./assets/images/projects/jawab/2.png";
+import jawab3 from "./assets/images/projects/jawab/3.png";
+import jawab4 from "./assets/images/projects/jawab/4.png";
+
+// king frog 
+import king_frog1 from "./assets/images/projects/king frog/1.jpg";
+import king_frog2 from "./assets/images/projects/king frog/2.jpg";
+
+//koshary woman
+import koshary_woman from "./assets/images/projects/koshary woman/كشرى وومان.jpg";
+import koshary_woman1 from "./assets/images/projects/koshary woman/1.jpg";
+import koshary_woman2 from "./assets/images/projects/koshary woman/2.jpg";
+
+// lost_city
+import lost_city1 from "./assets/images/projects/lost_city/1.jpg";
+import lost_city2 from "./assets/images/projects/lost_city/2.jpg";
+import lost_city3 from "./assets/images/projects/lost_city/3.jpg";
+import lost_city4 from "./assets/images/projects/lost_city/4.jpg";
+import lost_city5 from "./assets/images/projects/lost_city/5.jpg";
+import lost_city6 from "./assets/images/projects/lost_city/6.jpg";
+import lost_city7 from "./assets/images/projects/lost_city/7.jpg";
+
+// nour and the forest
+import nour_and_the_forest1 from "./assets/images/projects/nour and the forest/sarah_vector art _illustration_day1.png";
+import nour_and_the_forest2 from "./assets/images/projects/nour and the forest/sarah day2 forming.png";
+import nour_and_the_forest3 from "./assets/images/projects/nour and the forest/isometric-04.jpg";
+import nour_and_the_forest4 from "./assets/images/projects/nour and the forest/isometric-05.jpg";
+
+//spooky luna
+import spooky_luna1 from "./assets/images/projects/spooky luna/1.jpg";
+import spooky_luna2 from "./assets/images/projects/spooky luna/2.jpg";
+import spooky_luna3 from "./assets/images/projects/spooky luna/3.jpg";
+import spooky_luna4 from "./assets/images/projects/spooky luna/4.jpg";
+
+// stress
+import stress1 from "./assets/images/projects/stress/1.PNG";
+import stress2 from "./assets/images/projects/stress/2.png";
+import stress3 from "./assets/images/projects/stress/3.png";
+import stress4 from "./assets/images/projects/stress/4.GIF";
+
+// wedding 
+import wedding1 from "./assets/images/projects/wedding/1.png";
+import wedding2 from "./assets/images/projects/wedding/2.png";
+
+// whales in the sky
+import whales_in_the_sky1 from "./assets/images/projects/whales in the sky/1.PNG";
+import whales_in_the_sky2 from "./assets/images/projects/whales in the sky/2.PNG";
+import whales_in_the_sky3 from "./assets/images/projects/whales in the sky/3.JPG";
+import whales_in_the_sky4 from "./assets/images/projects/whales in the sky/4.JPG";
+const dataportfolio = [
+    {
+        id: '9',
+        img: jawab1,
+        description: "Jawab",
+        link: "#",
+    },
+    {
+        id: '6',
+        img: gang1,
+        description: "ITI Gang",
+        link: "#",
+    },
+    {
+        id: '8',
+        img: ghet_al_regal1,
+        description: "Ghet Al Regal",
+        link: "#",
+    },
+    {
+        id: '12',
+        img: lost_city1,
+        description: "Lost City",
+        link: "#",
+    },
+    {
+        id: '3',
+        img: china1,
+        description: "China",
+        link: "#",
+    },
+    {
+        id: '4',
+        img: diamondbox1,
+        description: "Diamond Box",
+        link: "#",
+    },
+    {
+        id: '17',
+        img: whales_in_the_sky1,
+        description: "Whales In The Sky",
+        link: "#",
+    },
+
+    {
+        id: '2',
+        img: bedr,
+        description: "Bedro",
+        link: "#",
+    },
+    {
+        id: '1',
+        img: balerina2,
+        description: "Balerina",
+        link: "#",
+    },
+    {
+        id: '5',
+        img: emily4,
+        description: "Emily",
+        link: "#",
+    },
+
+    {
+        id: '7',
+        img: gentle_meao1,
+        description: "Gentle Meao",
+        link: "#",
+    },
+
+    {
+        id: '10',
+        img: king_frog1,
+        description: "King Frog",
+        link: "#",
+    },
+    {
+        id: '16',
+        img: wedding1,
+        description: "Wedding",
+        link: "#",
+    },
+    {
+        id: '11',
+        img: koshary_woman1,
+        description: "Koshary Woman",
+        link: "#",
+    },
+
+    {
+        id: '13',
+        img: nour_and_the_forest2,
+        description: "Nour & The Forest",
+        link: "#",
+    },
+
+    {
+        id: '15',
+        img: stress1,
+        description: "Stress",
+        link: "#",
+    },
+
+    {
+        id: '14',
+        img: spooky_luna1,
+        description: "Spooky Luna",
+        link: "#",
+    },
+
 ];
 const dataportfolioDetails = {
     '1': [
         {
-            img: proj1_1,
+            img: balerina,
         },
         {
-            img: proj1_2,
+            img: balerina2,
         },
         {
-            img: proj1_3,
+            img: balerina3,
         },
-    ]
+    ],
+    '2': [
+        {
+            img: bedr,
+        },
+        {
+            img: bedr2,
+        },
+        {
+            img: bedr3,
+        },
+        {
+            img: bedr4,
+        },
+        {
+            img: bedr5,
+        },
+    ],
+    '3': [
+        {
+            img: china1,
+        },
+        {
+            img: china2,
+        },
+        {
+            img: china3,
+        },
+        {
+            img: china4,
+        },
+        {
+            img: china5,
+        },
+    ],
+    '4': [
+        {
+            img: diamondbox1,
+        },
+        {
+            img: diamondbox2,
+        },
+        {
+            img: diamondbox3,
+        },
+        {
+            img: diamondbox4,
+        },
+        {
+            img: diamondbox5,
+        },
+        {
+            img: diamondbox6,
+        },
+    ],
+    '5': [
+        {
+            img: emily1,
+        },
+        {
+            img: emily2,
+        },
+        {
+            img: emily3,
+        },
+        {
+            img: emily4,
+        },
+    ],
+    '6': [
+        {
+            img: gang1,
+        },
+        {
+            img: gang2,
+        },
+        {
+            img: gang3,
+        },
+        {
+            img: gang4,
+        },
+        {
+            img: gang5,
+        },
+        {
+            img: gang6,
+        },
+        {
+            img: gang7,
+        },
+        {
+            img: gang8,
+        },
+        {
+            img: gang9,
+        },
+        {
+            img: gang10,
+        },
+        {
+            img: gang11,
+        },
+        {
+            img: gang12,
+        },
+        {
+            img: gang13,
+        },
+        {
+            img: gang14,
+        },
+        {
+            img: gang15,
+        },
+        {
+            img: gang16,
+        },
+        {
+            img: gang17,
+        },
+        {
+            img: gang18,
+        },
+    ],
+    '7': [
+        {
+            img: gentle_meao1,
+        },
+        {
+            img: gentle_meao2,
+        },
+        {
+            img: gentle_meao3,
+        },
+        {
+            img: gentle_meao4,
+        },
+        {
+            img: gentle_meao5,
+        },
+        {
+            img: gentle_meao6,
+        },
+        {
+            img: gentle_meao7,
+        },
+    ],
+    '8': [
+        {
+            img: ghet_al_regal1,
+        },
+        {
+            img: ghet_al_regal2,
+        },
+        {
+            img: ghet_al_regal3,
+        },
+        {
+            img: ghet_al_regal4,
+        },
+        {
+            img: ghet_al_regal5,
+        },
+    ],
+    '9': [
+        {
+            img: jawab1,
+        },
+        {
+            img: jawab2,
+        },
+        {
+            img: jawab3,
+        },
+        {
+            img: jawab4,
+        },
+    ],
+    '10': [
+        {
+            img: king_frog1,
+        },
+        {
+            img: king_frog2,
+        },
+    ],
+    '11': [
+        {
+            img: koshary_woman,
+        },
+        {
+            img: koshary_woman1,
+        },
+        {
+            img: koshary_woman2,
+        },
+    ],
+    '12': [
+        {
+            img: lost_city1,
+        },
+        {
+            img: lost_city2,
+        },
+        {
+            img: lost_city3,
+        },
+        {
+            img: lost_city4,
+        },
+        {
+            img: lost_city5,
+        },
+        {
+            img: lost_city6,
+        },
+        {
+            img: lost_city7,
+        },
+    ],
+    '13': [
+        {
+            img: nour_and_the_forest1,
+        },
+        {
+            img: nour_and_the_forest2,
+        },
+        {
+            img: nour_and_the_forest3,
+        },
+        {
+            img: nour_and_the_forest4,
+        },
+    ],
+    '14': [
+        {
+            img: spooky_luna1,
+        },
+        {
+            img: spooky_luna2,
+        },
+        {
+            img: spooky_luna3,
+        },
+        {
+            img: spooky_luna4,
+        },
+    ],
+    '15': [
+        {
+            img: stress1,
+        },
+        {
+            img: stress2,
+        },
+        {
+            img: stress3,
+        },
+        {
+            img: stress4,
+        },
+    ],
+    '16': [
+        {
+            img: wedding1,
+        },
+        {
+            img: wedding2,
+        },
+    ],
+    '17': [
+        {
+            img: whales_in_the_sky1,
+        },
+        {
+            img: whales_in_the_sky2,
+        },
+        {
+            img: whales_in_the_sky3,
+        },
+        {
+            img: whales_in_the_sky4,
+        },
+    ],
+
 };
 const contactConfig = {
     YOUR_EMAIL: "sarahmohammed21fci@gmail.com",
