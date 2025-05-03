bbb21020134b708bf4b14582811e14d05c7f7b99
Abdulrahman Alaa Eldin <abdulrahmanalaa@hotmail.com>
2025-05-03 17:18:42 +0400

    refactor: Remove About page and related content from the project

--- src/content_option.ts
+++ src/content_option.ts
@@ -138,66 +138,6 @@ const introdata = {
     your_img_url: "https://drive.google.com/file/d/1kr2SLTrTtBbq9e6ucNI-c1u800pGaM1B/view?usp=sharing"
 };
 
-const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies."
-};
-const worktimeline = [
-    {
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020"
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019"
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019"
-    }
-];
-
-const skills = [
-    {
-        name: "Python",
-        value: 90
-    },
-    {
-        name: "Djano",
-        value: 85
-    },
-    {
-        name: "Javascript",
-        value: 80
-    },
-    {
-        name: "React",
-        value: 60
-    },
-    {
-        name: "Jquery",
-        value: 85
-    }
-];
-
-const services = [
-    {
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
-    }
-];
-
 const dataportfolio = [
     {
         id: '9',
@@ -615,12 +555,8 @@ const socialprofils = {
 };
 export {
     meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
     introdata,
+    dataportfolio,
     contactConfig,
     socialprofils,
     dataportfolioDetails,
